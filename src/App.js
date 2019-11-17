2ef3f1ca5a86ac5d60ab96b3f5a97d8aa1fa5d1f
patrickt26 <plthomaz@gmail.com>
2019-11-16 23:20:45 -0300

    Dados do Header

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyle />
-          <ToastContainer autoClose={5000} />
+          <ToastContainer autoClose={3000} />
         </Router>
       </PersistGate>
     </Provider>
